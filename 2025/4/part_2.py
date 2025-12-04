d51c756e93572f120e3c24fc386f4d4804dca265
Tobias Norlund <tobias@norlund.se>
2025-12-04 20:04:07 +0100

    4

---
+++ 2025/4/part_2.py
@@ -0,0 +1,52 @@
+from copy import deepcopy
+
+M = []  
+with open("input.txt") as f:
+    for line in f:
+        M.append([c for c in line.strip()])
+
+print(M)
+
+H, W = len(M), len(M[0])
+
+tot = 0
+s = 1
+while s > 0:
+    s = 0
+    M_ = deepcopy(M)
+    for y in range(H):
+        for x in range(W):
+            m = 0
+            # nw
+            if 0 <= y-1 and 0 <= x-1 and M[y-1][x-1] == "@":
+                m += 1
+            # n
+            if 0 <= y-1 and M[y-1][x] == "@":
+                m += 1
+            # ne
+            if 0 <= y-1 and 0 <= x+1 < W and M[y-1][x+1] == "@":
+                m += 1
+            # w
+            if 0 <= y < H and 0 <= x-1 < W and M[y][x-1] == "@":
+                m += 1
+            # e
+            if 0 <= y < H and 0 <= x+1 < W and M[y][x+1] == "@":
+                m += 1
+            # sw
+            if 0 <= y+1 < H and 0 <= x-1 < W and M[y+1][x-1] == "@":
+                m += 1
+            # s
+            if 0 <= y+1 < H and 0 <= x < W and M[y+1][x] == "@":
+                m += 1
+            # se
+            if 0 <= y+1 < H and 0 <= x+1 < W and M[y+1][x+1] == "@":
+                m += 1
+            
+            if m < 4 and M[y][x] == "@":
+                s += 1
+                M_[y][x] = "."
+    
+    M = M_
+    tot += s
+
+print(tot)
